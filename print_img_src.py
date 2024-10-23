55973f364fcc2eefdc0aea88c48ee3bf581c8a31
Brice Chivu <bricechivu@gmail.com>
2024-10-23 19:53:03 +0900

    copy and print only not yet commited pictures

--- print_img_src.py
+++ print_img_src.py
@@ -1,26 +1,47 @@
+import subprocess
 from pathlib import Path
 
 import pyperclip
-
-# Usage: python3 print_img_src.py
 from PIL import Image
+
+
+def get_untracked_files():
+    # Get the list of untracked files using git
+    result = subprocess.run(
+        ["git", "ls-files", "--others", "--exclude-standard"],
+        capture_output=True,
+        text=True,
+    )
+    # Split the output by lines to get each untracked file
+    untracked_files = result.stdout.splitlines()
+    return set(untracked_files)
 
 
 def print_img_src():
     path = Path("assets/photos").resolve()
     if not path.exists():
         print(f"path:{path} is wrong")
-    html_ouput = ""
+        return
+
+    # Get the set of untracked files
+    untracked_files = get_untracked_files()
+
+    html_output = ""
     for filepath in sorted(
         path.glob("*"), key=lambda x: "_".join(str(x).split("_")[2:])
     ):
-        if any(ext in str(filepath).lower() for ext in ["jpg", "jpeg", "png"]):
+        name = str(filepath).split("Photo/")[1]
+        # Check if the file is an image and is untracked by Git
+        if (
+            any(ext in str(filepath).lower() for ext in ["jpg", "jpeg", "png"])
+            and name in untracked_files
+        ):
             img = Image.open(filepath)
-            name = str(filepath).split("Photo/")[1]
             html_line = f'<img src="{name}" width="{img.width}" height="{img.height}" loading="lazy" class="thumbnail" onclick="toggleScale(this)"/>'
-            html_ouput += html_line + "\n"
-    print(html_ouput)
-    pyperclip.copy(html_ouput)
+            html_output += html_line + "\n"
+
+    print(html_output)
+    pyperclip.copy(html_output)
 
 
 if __name__ == "__main__":

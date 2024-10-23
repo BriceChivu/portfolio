b4987f2a1a039b3a1fa670037f0923b47958ce06
Brice Chivu <bricechivu@gmail.com>
2024-10-23 22:05:47 +0900

    chatGPT modified

--- print_html.py
+++ print_html.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pyperclip
 from PIL import Image
 
-# Usage: python assets/python/print_html.py
+# Usage: python print_html.py
 
 
 def get_untracked_files():
@@ -20,31 +20,32 @@ def get_untracked_files():
 
 
 def print_img_src():
-    # Get the set of untracked files
     untracked_files = get_untracked_files()
-
     html_output = ""
     path = Path("assets/photos").resolve()
+
     for filepath in sorted(
         path.glob("*"), key=lambda x: "_".join(str(x).split("_")[2:])
     ):
         img_path = str(filepath).split("Photo/")[1]
-        # Check if the file is an image and is untracked by Git
         if (
             any(
-                ext in str(filepath).lower()
-                for ext in ["jpg", "jpeg", "png", "tiff", "heic"]
+                ext in filepath.suffix.lower()
+                for ext in [".jpg", ".jpeg", ".png", ".tiff", ".heic"]
             )
-            # and img_path in untracked_files
+            and img_path in untracked_files
         ):
-            img_name = img_path.split("/")[-1].split(".")[-2]
-            for low_filepath in Path("assets/photos/low").resolve().glob("*"):
-                if img_name in str(low_filepath):
-                    low_img_path = str(low_filepath).split("Photo/")[1]
+            img_name = filepath.stem
+            low_img_path = next(
+                (
+                    str(p).split("Photo/")[1]
+                    for p in Path("assets/photos/low").resolve().glob("*")
+                    if img_name in str(p)
+                ),
+                "",
+            )
             img = Image.open(filepath)
-            html_line = f'<img src="{low_img_path}" data-fullres="{img_path}" width="{img.width}" height="{img.height}" class="thumbnail" onclick="toggleScale(this)"/>'
-
-            html_output += html_line + "\n"
+            html_output += f'<img src="{low_img_path}" data-fullres="{img_path}" width="{img.width}" height="{img.height}" class="thumbnail" onclick="toggleScale(this)"/>\n'
 
     print(html_output)
     pyperclip.copy(html_output)

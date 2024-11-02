b91b56eb26f427a792540e3b9c3a04875cf522d4
Brice Chivu <bricechivu@gmail.com>
2024-11-02 21:48:32 +0800

    fixed scaling effect

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// script.js
-
-window.addEventListener('load', function() {
-  // Get all images with the 'thumbnail' class
-  const thumbnails = document.querySelectorAll('img.thumbnail');
-
-  thumbnails.forEach((img) => {
-    const highResUrl = img.getAttribute('data-fullres');
-    loadHighResImage(img, highResUrl);
-  });
-});
-
-function loadHighResImage(elem, highResUrl) {
-  let image = new Image();
-  image.addEventListener('load', () => elem.src = highResUrl); // Swap to high-res when it's fully loaded
-  image.src = highResUrl;
-}
-
 function toggleScale(element) {
-    element.classList.toggle("scaled");
-  }
+    if (element.naturalWidth > element.naturalHeight) {
+        element.classList.toggle("horizontal");
+    } else {
+        element.classList.toggle("vertical");
+    }
+}

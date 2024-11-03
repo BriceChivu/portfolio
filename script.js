903d08b8a7d28877e1a650320044db7ca6fafb0e
Brice Chivu <bricechivu@gmail.com>
2024-11-03 21:18:57 +0800

    some changes

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function toggleScale(element) {
-    if (element.naturalWidth > element.naturalHeight) {
-        element.classList.toggle("horizontal");
-    } else {
-        element.classList.toggle("vertical");
-    }
-}
+    element.classList.toggle("scaled");
+  }

6ad4244349b2e9c6b3650fbacfaef278bf846b94
Brice Chivu <bricechivu@gmail.com>
2024-11-21 11:24:09 +0700

    test

--- print_html.py
+++ print_html.py
@@ -22,10 +22,22 @@ def get_untracked_files():
 def print_img_src():
     untracked_files = get_untracked_files()
     html_output = ""
-    path = Path("assets/photos").resolve()
+    path = Path("assets/photos/mog").resolve()
 
     for filepath in sorted(
-        path.glob("*"), key=lambda x: "_".join(str(x).split("_")[2:])
+        path.glob("*"),
+        key=lambda x: (
+            str(x).split("_")[1]
+            if len(str(x).split("_")) > 1
+            else "",  # Sort by location (e.g., 'busan', 'seoul')
+            str(x).split("_")[2]
+            if len(str(x).split("_")) > 2
+            else "",  # Sort by the next part (e.g., '00018' or 'qingdao')
+            int(str(x).split("_")[3].split(".")[0])
+            if len(str(x).split("_")) > 3
+            and str(x).split("_")[3].split(".")[0].isdigit()
+            else float("inf"),
+        ),
     ):
         img_path = str(filepath).split("portfolio/")[1]
         if (
@@ -36,7 +48,7 @@ def print_img_src():
             and img_path in untracked_files
         ):
             img = Image.open(filepath)
-            html_output += f'<img src="{img_path}" width="{img.width}" height="{img.height}" class="thumbnail" onclick="toggleScale(this)"/>\n'
+            html_output += f'<img src="{img_path}" width="{img.width}" height="{img.height}" class="thumbnail" onclick="toggleScale(this)" loading="lazy"/>\n'
 
     print(html_output)
     pyperclip.copy(html_output)
